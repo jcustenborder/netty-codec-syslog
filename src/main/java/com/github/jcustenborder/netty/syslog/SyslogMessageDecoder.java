cea44d8756e8f8efeb8480bebfad26a9f3991fd3
Jeremy Custenborder <jcustenborder@gmail.com>
2018-02-23 15:49:10 -0600

    Removed unused import.

--- src/main/java/com/github/jcustenborder/netty/syslog/SyslogMessageDecoder.java
+++ src/main/java/com/github/jcustenborder/netty/syslog/SyslogMessageDecoder.java
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 public class SyslogMessageDecoder extends MessageToMessageDecoder<SyslogRequest> {
