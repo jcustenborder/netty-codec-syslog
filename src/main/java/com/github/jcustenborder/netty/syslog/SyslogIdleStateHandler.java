c05fa3c4ed4229dd4c3ce46dccbd8ca91a3bb0b7
Jeremy Custenborder <jcustenborder@gmail.com>
2018-08-03 20:16:28 -0700

    Added support to detect idle sessions and terminate them. Fixes #24. (#25)
    * Added support to detect idle sessions and terminate them. Fixes #24.
    
    * Updated license header.

---
+++ src/main/java/com/github/jcustenborder/netty/syslog/SyslogIdleStateHandler.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright © 2018 Jeremy Custenborder (jcustenborder@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jcustenborder.netty.syslog;
+
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ChannelHandler.Sharable
+class SyslogIdleStateHandler extends ChannelDuplexHandler {
+  private static final Logger log = LoggerFactory.getLogger(SyslogIdleStateHandler.class);
+
+  public static final SyslogIdleStateHandler INSTANCE = new SyslogIdleStateHandler();
+
+  private SyslogIdleStateHandler() {
+
+  }
+
+  @Override
+  public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+    if (evt instanceof IdleStateEvent) {
+      IdleStateEvent e = (IdleStateEvent) evt;
+      if (e.state() == IdleState.WRITER_IDLE) {
+        log.info("Disconnecting {} due to idle timeout.", ctx.channel().remoteAddress());
+        ctx.close();
+      }
+    }
+  }
+}
